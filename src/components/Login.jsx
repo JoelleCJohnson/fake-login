54f8db1b04475e157eb7fc70e929989ababa95c2
Joelle  Johnson <joellej221@gmail.com>
2023-10-31 12:16:26 -0400

    added handleLoginForm function

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react"
 export default function Login() {
 
     const[isLoggedIn, setIsLoggedIn] = useState(false)
+    const [error, setError] = useState()
 
     useEffect(() => {
         if(isLoggedIn){
@@ -14,13 +15,18 @@ export default function Login() {
 
     const handleLoginForm = (e) => {
         e.preventDefault()
+        const email = e.target.email.value
+        const password = e.target.password.value
+        if(email === 'joellej221@gmail.com' && password === '123456'){ // we would NEVER hardcode this password in the real world. Not secure.
+            setIsLoggedIn(true)
+        }
     }
 
     return (
         <main>
             <h2>Log in</h2>
             <section className="login-form">
-                <form onSubmit={() => handleLoginForm}>
+                <form onSubmit={handleLoginForm}>
 
                     <label htmlFor="email">
                         Email:
